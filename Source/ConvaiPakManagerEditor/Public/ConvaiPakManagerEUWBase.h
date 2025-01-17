21f20526a051b8fb79eb204ed6e234c6f397d071
Anmol <anmol@convai.com>
2025-01-17 16:27:28 +0530

    feat : Add pak manager editor utility widget base

---
+++ Source/ConvaiPakManagerEditor/Public/ConvaiPakManagerEUWBase.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorUtilityWidget.h"
+#include "ConvaiPakManagerEUWBase.generated.h"
+
+
+UCLASS(Abstract)
+class CONVAIPAKMANAGEREDITOR_API UConvaiPakManagerEUWBase : public UEditorUtilityWidget
+{
+	GENERATED_BODY()
+
+};
