e701fc8a24876bd5e8f15555f4aae54ed6efae7a
Anmol <anmol@convai.com>
2025-01-17 16:25:17 +0530

    feat : Add Pak utility func lib

---
+++ Source/ConvaiPakManager/Private/ConvaiPakUtilityLibrary.cpp
@@ -0,0 +1,81 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ConvaiPakUtilityLibrary.h"
+
+#include "DesktopPlatformModule.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+
+bool UConvaiPakUtilityLibrary::IsFileValid(const FString& FilePath)
+{
+	return FPlatformFileManager::Get().GetPlatformFile().FileExists(*FilePath);
+}
+
+bool UConvaiPakUtilityLibrary::LoadFileToByteArray(const FString& FilePath, TArray<uint8>& FileData)
+{
+	if (!FFileHelper::LoadFileToArray(FileData, *FilePath))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to load file: %s"), *FilePath);
+		return false;
+	}
+	return true;
+}
+
+FString UConvaiPakUtilityLibrary::OpenFileDialog(const TArray<FString>& Extensions)
+{
+	FString FilePath;
+	FString FileTypes = TEXT("Supported Files (");
+	
+	// Validate input
+	if (Extensions.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Extensions array is empty"));
+		FileTypes = TEXT("All Files (*.*)|*.*");
+	}
+	else
+	{
+		// Construct file filter string
+		for (int32 Index = 0; Index < Extensions.Num(); ++Index)
+		{
+			if (Index > 0)
+			{
+				FileTypes += TEXT(";");
+			}
+			FileTypes += TEXT("*.") + Extensions[Index];
+		}
+	
+		FileTypes += TEXT(")|");
+		for (int32 Index = 0; Index < Extensions.Num(); ++Index)
+		{
+			if (Index > 0)
+			{
+				FileTypes += TEXT(";");
+			}
+			FileTypes += TEXT("*.") + Extensions[Index];
+		}
+	}	
+	
+	// Get the Desktop Platform module
+	if (IDesktopPlatform* DesktopPlatform = FDesktopPlatformModule::Get())
+	{
+		// File Dialog Options
+		const void* ParentWindowHandle = FSlateApplication::Get().FindBestParentWindowHandleForDialogs(nullptr);
+		TArray<FString> OutFiles;
+		const bool bOpened = DesktopPlatform->OpenFileDialog(
+			ParentWindowHandle,
+			TEXT("Choose a file"),             // Dialog title
+			FPaths::ProjectDir(),					// Default path
+			TEXT(""),								// Default file
+			FileTypes,								// File types
+			EFileDialogFlags::None,
+			OutFiles
+		);
+
+		if (bOpened && OutFiles.Num() > 0)
+		{
+			FilePath = OutFiles[0]; 
+		}
+	}
+
+	return FilePath;
+}
---
+++ Source/ConvaiPakManager/Public/ConvaiPakUtilityLibrary.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ConvaiPakUtilityLibrary.generated.h"
+
+
+UCLASS()
+class CONVAIPAKMANAGER_API UConvaiPakUtilityLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Convai|PakManager")
+	static bool IsFileValid(const FString& FilePath);
+
+	UFUNCTION(BlueprintCallable, Category = "Convai|PakManager")
+	static bool LoadFileToByteArray(const FString& FilePath, TArray<uint8>& FileData);
+	
+	UFUNCTION(BlueprintCallable, Category = "Convai|PakManager")
+	static FString OpenFileDialog(const TArray<FString>& Extensions);
+};
