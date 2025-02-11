441d0510d714ccd5c45f539d716023d900c438c5
Anmol <anmol@convai.com>
2025-02-11 12:20:24 +0530

    feat: Add Pak Editor Subsystem

---
+++ Source/ConvaiPakManagerEditor/Private/ConvaiPakEditorSubsystem.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ConvaiPakEditorSubsystem.h"
+#include "EditorUtilityLibrary.h"
+
+void UConvaiPakEditorSubsystem::GetSelectedAssetData(FString& PackageName)
+{
+	TArray<FAssetData> SelectedAssets = UEditorUtilityLibrary::GetSelectedAssetData();
+	if (SelectedAssets.Num() > 0)
+	{
+		PackageName = SelectedAssets[0].PackageName.ToString();
+	}
+}
---
+++ Source/ConvaiPakManagerEditor/Public/ConvaiPakEditorSubsystem.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EditorSubsystem.h"
+#include "ConvaiPakEditorSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONVAIPAKMANAGEREDITOR_API UConvaiPakEditorSubsystem : public UEditorSubsystem
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = "Convai|Pak")
+	void GetSelectedAssetData(FString& PackageName);
+};
