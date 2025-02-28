5cdeae71350ce94bef082a9a7a0891a85720776b
Anmol <anmol@convai.com>
2025-02-28 09:27:05 +0530

    feat: Add logging utilities

---
+++ Source/ConvaiPakManager/Private/Utility/CPM_Log.cpp
@@ -0,0 +1,6 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Utility/CPM_Log.h"
+
+DEFINE_LOG_CATEGORY(ConvaiPakManagerLog);
---
+++ Source/ConvaiPakManager/Public/Utility/CPM_Log.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(ConvaiPakManagerLog, Log, All);
+
+// Use the macro from the Build.cs definition
+#if CONVAI_PAK_MANAGER_LOG
+#define CPM_LOG(Verbosity, Format, ...) UE_LOG(ConvaiPakManagerLog, Verbosity, Format, ##__VA_ARGS__)
+#else
+#define CPM_LOG(Verbosity, Format, ...)
+#endif
