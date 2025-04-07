d8324dbbb17196fad2dbc5e7682a680e1e7643e4
Anmol <anmol@convai.com>
2025-04-07 20:27:39 +0530

    chore: Add func to mark package dirty

--- Source/ConvaiPakManagerEditor/Private/ConvaiPakManagerEditorUtils.cpp
+++ Source/ConvaiPakManagerEditor/Private/ConvaiPakManagerEditorUtils.cpp
@@ -2,3 +2,26 @@
 
 
 #include "ConvaiPakManagerEditorUtils.h"
+#include "AssetRegistry/AssetRegistryModule.h"
+#include "Misc/PackageName.h"
+#include "UObject/Package.h"
+
+void UConvaiPakManagerEditorUtils::CPM_MarkAssetDirty(UObject* Asset)
+{
+	if (!Asset)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MarkAssetDirty: Asset is null."));
+		return;
+	}
+
+	UPackage* Package = Asset->GetOutermost();
+	if (Package)
+	{
+		Package->SetDirtyFlag(true);
+		FAssetRegistryModule::AssetCreated(Asset);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("MarkAssetDirty: Could not get package for asset '%s'."), *Asset->GetName());
+	}
+}
--- Source/ConvaiPakManagerEditor/Public/ConvaiPakManagerEditorUtils.h
+++ Source/ConvaiPakManagerEditor/Public/ConvaiPakManagerEditorUtils.h
@@ -11,6 +11,10 @@ UCLASS()
 class CONVAIPAKMANAGEREDITOR_API UConvaiPakManagerEditorUtils : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+public:
+	/** Marks the given asset as dirty so it can be saved */
+	UFUNCTION(BlueprintCallable, Category = "Convai|PakManager")
+	static void CPM_MarkAssetDirty(UObject* Asset);	
 };
  
