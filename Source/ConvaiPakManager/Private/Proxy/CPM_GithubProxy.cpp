f786ff71809558aa45e70d98a908c22d92e48fe9
Anmol <anmol@convai.com>
2025-04-28 15:53:56 +0530

    feat: Add proxy to get file from git repo

---
+++ Source/ConvaiPakManager/Private/Proxy/CPM_GithubProxy.cpp
@@ -0,0 +1,44 @@
+﻿// Convai
+
+
+#include "Proxy/CPM_GithubProxy.h"
+
+UCPM_GetGithubRepoFileProxy* UCPM_GetGithubRepoFileProxy::GetGithubRepoFileProxy(
+	const FString& GithubRepoName, const FString& BranchName, const FString& FileName)
+{
+	UCPM_GetGithubRepoFileProxy* Proxy = NewObject<UCPM_GetGithubRepoFileProxy>();
+	Proxy->URL = FString::Printf(TEXT("https://raw.githubusercontent.com/%s/%s/%s"), *GithubRepoName, * BranchName, *FileName);	
+	return Proxy;
+}
+
+bool UCPM_GetGithubRepoFileProxy::ConfigureRequest(TSharedRef<IConvaihttpRequest> Request, const TCHAR* Verb)
+{
+	if (!Super::ConfigureRequest(Request, ConvaiHttpConstants::GET))
+	{
+		return false;
+	}
+	
+	// Request->OnRequestProgress().BindLambda(
+	// [&](CONVAI_HTTP_REQUEST_PTR InRequest, CONVAI_HTTP_DOWN_PROGRESS_TYPE BytesSent, CONVAI_HTTP_DOWN_PROGRESS_TYPE BytesReceived)
+	// {
+	// 	const uint64 TotalBytes = InRequest->GetResponse()->GetContentLength();
+	// 	const float UploadProgress = TotalBytes > 0 ? static_cast<float>(BytesSent) / static_cast<float>(TotalBytes) : 0.0f;
+	//
+	// 	OnProgress.Broadcast(ResponseString, UploadProgress);
+	// });
+	
+	return true;
+}
+
+void UCPM_GetGithubRepoFileProxy::HandleSuccess()
+{
+	Super::HandleSuccess();
+	OnSuccess.Broadcast(ResponseString);
+}
+
+void UCPM_GetGithubRepoFileProxy::HandleFailure()
+{
+	Super::HandleFailure();
+	OnFailure.Broadcast(ResponseString);
+}
+
---
+++ Source/ConvaiPakManager/Public/Proxy/CPM_GithubProxy.h
@@ -0,0 +1,30 @@
+﻿// Convai
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RestAPI/ConvaiAPIBase.h"
+#include "CPM_GithubProxy.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCPM_OnGetGithubRepoFile, const FString&, ResponseString);
+
+UCLASS()
+class UCPM_GetGithubRepoFileProxy : public UConvaiAPIBaseProxy
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FCPM_OnGetGithubRepoFile OnSuccess;
+	
+	UPROPERTY(BlueprintAssignable)
+	FCPM_OnGetGithubRepoFile OnFailure;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", DisplayName = "Get Github Repo file" , WorldContext = "WorldContextObject"), Category = "Convai|PakManager")
+	static UCPM_GetGithubRepoFileProxy* GetGithubRepoFileProxy(const FString& GithubRepoName, const FString& BranchName, const FString& FileName);
+
+protected:
+	virtual bool ConfigureRequest(TSharedRef<CONVAI_HTTP_REQUEST_INTERFACE> Request, const TCHAR* Verb) override;
+	virtual void HandleSuccess() override;
+	virtual void HandleFailure() override;
+};
