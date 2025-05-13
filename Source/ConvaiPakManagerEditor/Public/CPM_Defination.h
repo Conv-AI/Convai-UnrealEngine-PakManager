d66990824455bfd9701deff276ee3a2cdcca8c23
Anmol <anmol@convai.com>
2025-05-13 19:57:37 +0530

    feat: Add package param struct

---
+++ Source/ConvaiPakManagerEditor/Public/CPM_Defination.h
@@ -0,0 +1,27 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CPM_Defination.generated.h"
+
+USTRUCT(BlueprintType)
+struct FCPM_PackageParam
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Convai|PakManager")
+	FString Platform;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Convai|PakManager")
+	FString Configuration;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Convai|PakManager")
+	FString OutputDirectory;
+
+	bool IsValid() const
+	{		
+		return !Platform.IsEmpty()
+			&& !Configuration.IsEmpty()
+			&& !OutputDirectory.IsEmpty();
+	}
+};
