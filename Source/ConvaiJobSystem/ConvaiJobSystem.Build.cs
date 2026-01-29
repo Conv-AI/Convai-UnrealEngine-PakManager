a7c16722c4185613dd359912453512f1e70c8cbc
AnmolConvai <anmol@convai.com>
2026-01-29 13:32:57 +0530

    feat: Add Job system module

---
+++ Source/ConvaiJobSystem/ConvaiJobSystem.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class ConvaiJobSystem : ModuleRules
+{
+    public ConvaiJobSystem(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/ConvaiJobSystem/Private/ConvaiJobSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "ConvaiJobSystem.h"
+
+#define LOCTEXT_NAMESPACE "FConvaiJobSystemModule"
+
+void FConvaiJobSystemModule::StartupModule()
+{
+    
+}
+
+void FConvaiJobSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FConvaiJobSystemModule, ConvaiJobSystem)
